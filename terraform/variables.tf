4f4214d84ff66ade4e68085f4ef6361553335d7c
Roig <Natalia.Roig@proveedoresraizen.com>
2025-06-07 15:57:59 -0300

    Agrego variables.tf con valores por defecto para ubicación y cantidad de nodos

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  description = "Ubicación del recurso"
+  default     = "East US"
+}
+
+variable "node_count" {
+  description = "Cantidad de nodos en el node pool"
+  default     = 2
+}
